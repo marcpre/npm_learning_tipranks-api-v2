8e404aee1002f6b52f73ee9b3f3e6bd656aaaea8
marcpre <marcus.presich@gmail.com>
2019-09-23 06:46:31 +0200

    setup tiprank api

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # npm_learning_tipranks-api-v2
+
+## API
+
+[npm package - tipranks-api-v2](https://www.npmjs.com/package/tipranks-api-v2)
---
+++ src/t01-basic.js
@@ -0,0 +1,7 @@
+const tipranksApi = require('tipranks-api-v2');
+
+tipranksApi.getPriceTargets('TSLA').then(result => console.log(result));
+
+tipranksApi.getNewsSentimentData('TSLA').then(result => console.log(result));
+
+tipranksApi.getTrendingStocks().then(console.log);
